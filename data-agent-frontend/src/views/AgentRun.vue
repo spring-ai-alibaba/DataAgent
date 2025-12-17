bcca8ba909d29bfa03c3f4671262f8e5dd256271
VLSMB <2047857654@qq.com>
2025-12-18 01:03:22 +0800

    fix ci

--- data-agent-frontend/src/views/AgentRun.vue
+++ data-agent-frontend/src/views/AgentRun.vue
@@ -761,10 +761,10 @@
 
         // 去除可能的Markdown前后缀
         if (content.startsWith('```html')) {
-          content = content.substring(7)
+          content = content.substring(7);
         }
-        if (content.endsWith("```")) {
-          content = content.substring(0, content.length - 3)
+        if (content.endsWith('```')) {
+          content = content.substring(0, content.length - 3);
         }
 
         const blob = new Blob([content], { type: 'text/html' });
