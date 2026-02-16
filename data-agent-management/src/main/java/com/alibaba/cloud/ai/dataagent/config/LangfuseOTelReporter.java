c3f471922bd5513509d4066575d7c133798d46f3
zihenzzz <1624252184@qq.com>
2026-02-16 16:59:04 +0800

    feat: add langfuse

--- data-agent-management/src/main/java/com/alibaba/cloud/ai/dataagent/config/LangfuseOTelReporter.java
+++ data-agent-management/src/main/java/com/alibaba/cloud/ai/dataagent/config/LangfuseOTelReporter.java
@@ -27,7 +27,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author zihenzzz
